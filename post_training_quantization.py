b84d0e4b3de6de28846fa9eaa6ce20028a410323
PRJCT <m1xst99@yandex.ru>
2020-03-29 20:05:51 +0300

    minor enhancement

--- post_training_quantization.py
+++ post_training_quantization.py
@@ -3,6 +3,7 @@ from torch.utils.data import DataLoader
 from torchvision.datasets import ImageFolder
 from torchvision import transforms
 from faced_model import FacedModel
+from emorec_model import MiniXception
 import os
 import numpy as np
 import time
@@ -21,16 +22,16 @@ def main(task, model_name, img_size, model_params):
 	batch_size = 1
 	num_calibration_batches = 10
 
-	transforms = transforms.Compose([transforms.Resize(img_size),
+	transform = transforms.Compose([transforms.Resize(img_size),
 	                             ImageToTensor()])
 
-	dataset = ImageFolder(PATH_TO_DATA, transform=transforms)
+	dataset = ImageFolder(PATH_TO_DATA, transform=transform)
 	dataloader = DataLoader(dataset, batch_size=batch_size)
 
 	if task == 'detection':
 		model = FacedModel(*model_params)
 	elif task == 'classification':
-		model = MiniXception(*model_params)
+		model = MiniXception(['Anger', 'Happy', 'Neutral', 'Surprise'], *model_params)
 
 	load = torch.load(os.path.join(PATH_TO_MODEL, model_name), map_location='cpu')
 	model.load_state_dict(load['model_state_dict'])
@@ -64,7 +65,7 @@ if __name__ == '__main__':
 	TASK = sys.argv[1]
 	MODEL_NAME = sys.argv[2]
 	IMG_SIZE = int(sys.argv[3]), int(sys.argv[3])
-	MODEL_PARAMS = list(map(int, sys.argv[4]))
+	MODEL_PARAMS = list(map(int, sys.argv[4].split()))
 
 	main(task=TASK, model_name=MODEL_NAME, img_size=IMG_SIZE, model_params=MODEL_PARAMS)
 
